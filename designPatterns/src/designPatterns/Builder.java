dabb6f3a76a280ac3245128359e392dc47ec9977
Jahed <jahednaghipoor1361@gmail.com>
2020-05-11 20:25:50 +0200

    The first commit and push

---
+++ designPatterns/src/designPatterns/Builder.java
@@ -0,0 +1,17 @@
+package designPatterns;
+
+public class Builder {
+
+	public static void main(String[] args) {
+	
+		System.out.println("This is a builder pattern");
+		
+		StringBuilder sb = new StringBuilder();
+		
+		Object a = null;
+		sb.append(a);
+		Object b = null;
+		sb.append(b);
+	}
+
+}
