10cc8b26d0aa999fc159bc7afd8b4e5857d2e076
Jahed <jahednaghipoor1361@gmail.com>
2020-05-13 23:43:26 +0200

    Prototype (cloneable)

---
+++ designPatterns/src/PrototypeCloneable/Address.java
@@ -0,0 +1,29 @@
+package PrototypeCloneable;
+
+public class Address implements Cloneable{
+	
+	public String streetName;
+	public int houseNumber;
+	
+	
+	public Address(String streetName, int houseNumber) {
+		super();
+		this.streetName = streetName;
+		this.houseNumber = houseNumber;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Address [streetName=" + streetName + ", houseNumber=" + houseNumber + "]";
+	}
+	
+	// deep copy
+	@Override
+	public Object clone() throws CloneNotSupportedException {
+		// TODO Auto-generated method stub
+		return new Address(streetName, houseNumber);
+	}
+	
+
+}
